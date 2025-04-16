c9e73d39cad75e7ec86f1db14b54e1048fa72910
Perlou <perloukevin@gmail.com>
2025-04-16 23:45:40 +0800

    feat router

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/e421083458/golang_common/lib"
+	"github.com/perlou/gin-scaffold/router"
+)
+
+func main() {
+	lib.InitModule("./conf/dev/", []string{"base", "mysql", "redis"})
+	defer lib.Destroy()
+	router.HttpServerRun()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	router.HttpServerStop()
+}
